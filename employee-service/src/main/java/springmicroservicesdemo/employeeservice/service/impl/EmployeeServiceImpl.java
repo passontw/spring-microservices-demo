57fc533df614037e602bd582680afceb8c5abf86
PassonoTW <82664528+passontw@users.noreply.github.com>
2024-06-25 01:12:44 +0800

    use webclient

--- employee-service/src/main/java/springmicroservicesdemo/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/springmicroservicesdemo/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 import springmicroservicesdemo.employeeservice.dto.APIResponseDto;
 import springmicroservicesdemo.employeeservice.dto.DepartmentDto;
 import springmicroservicesdemo.employeeservice.dto.EmployeeDto;
@@ -19,7 +20,8 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
 
-    private RestTemplate restTemplate;
+//    private RestTemplate restTemplate;
+    private WebClient webClient;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -35,9 +37,10 @@ public class EmployeeServiceImpl implements EmployeeService {
                 () -> new ResourceNotFoundException("Employee", "id", employeeId)
         );
 
-        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8100/api/departments/" + employee.getDepartmentCode(), DepartmentDto.class);
-        DepartmentDto departmentDto = responseEntity.getBody();
+//        ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity("http://localhost:8100/api/departments/" + employee.getDepartmentCode(), DepartmentDto.class);
+//        DepartmentDto departmentDto = responseEntity.getBody();
 
+        DepartmentDto departmentDto = webClient.get().uri("http://localhost:8100/api/departments/" + employee.getDepartmentCode()).retrieve().bodyToMono(DepartmentDto.class).block();
         EmployeeDto employeeDto = AutoEmployeeMapper.MAPPER.mapEmployeeToEmployeeDto(employee);
 
         APIResponseDto apiResponseDto = new APIResponseDto();
