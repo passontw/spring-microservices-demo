592dcae9d8e1a7627b6678df6e93a6977fb47111
TomasLin <horsekit1982@gmail.com>
2024-06-25 15:24:18 +0800

    eureka load balancer

--- employee-service/src/main/java/springmicroservicesdemo/employeeservice/service/APIClient.java
+++ employee-service/src/main/java/springmicroservicesdemo/employeeservice/service/APIClient.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import springmicroservicesdemo.employeeservice.dto.DepartmentDto;
 
-@FeignClient(url = "http://localhost:8100", value = "DEPARTMENT-SERVICE")
+@FeignClient(name = "DEPARTMENT-SERVICE")
 public interface APIClient {
     @GetMapping("api/departments/{departmentCode}")
     public DepartmentDto getDepartment(@PathVariable String departmentCode);
